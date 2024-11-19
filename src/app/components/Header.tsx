915f56013c70e37ce70227db30b0318334ad5048
maimoona <maimoonamunir2023@gmail.com>
2024-11-18 23:48:23 -0800

    initial commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,31 @@
 import Link from "next/link";
-export default function Header(data: any) {
-    return (
-     <>
-     <div className={`${data.font.className} links-parent`}>
-        <ul className="links">
-          <li className="link"><Link href="">Works</Link></li>
-          <li className="link"><Link href="">Blog</Link></li>
-          <li className="link"><Link href="">Contact</Link></li>
-        </ul>
-     </div>
-     </>
-    );
-  }
+
+interface HeaderProps {
+  data?: {
+    font?: {
+      className?: string;
+    };
+  };
+}
+
+export default function Header({ data }: HeaderProps) {
+  const className = data?.font?.className || ""; // Use a fallback value if undefined
+
+  return (
+    <div className={`${className} links-parent`}>
+      <ul className="links">
+        <li className="link">
+          <Link href="">Works</Link>
+        </li>
+        <li className="link">
+          <Link href="">Blog</Link>
+        </li>
+        <li className="link">
+          <Link href="">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
   
