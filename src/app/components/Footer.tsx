951cb46115345498ebb4da344c2217f479193e43
maimoona <maimoonamunir2023@gmail.com>
2024-11-19 00:06:19 -0800

    third commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,17 +1,17 @@
 
+
 export default function Footer() {
   return (
     <>
       <div className="footer">
-        <div className="iconsdiv">
-        <i className="bi bi-facebook"></i>
-        <i className="bi bi-instagram"></i>
-        <i className="bi bi-twitter"></i>
-        <i className="bi bi-linkedin"></i>
-        </div>
-        <p className="copyright">
-          Copyright © | 2024 All rights reserved
-        </p>
+          <div className="iconsDiv">
+              <i className="bi bi-facebook"></i>
+              <i className="bi bi-instagram"></i>
+              <i className="bi bi-twitter"></i>
+              <i className="bi bi-linkedin"></i>
+          </div>
+
+          <p className="copyright">Copyright ©2020 All rights reserved.</p>
       </div>
     </>
   );
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,27 +1,23 @@
 import Link from "next/link";
-import { NextFont } from "next/font/google"; // Import the type for better typing
 
 interface HeaderProps {
-  font: NextFont; // Expect a `font` prop of type `NextFont`
+  font: {
+    className: string;
+  };
 }
 
 export default function Header({ font }: HeaderProps) {
-  return (
-    <div className={`${font.className} links-parent`}>
-      <ul className="links">
-        <li className="link">
-          <Link href="">Works</Link>
-        </li>
-        <li className="link">
-          <Link href="">Blog</Link>
-        </li>
-        <li className="link">
-          <Link href="">Contact</Link>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-
+    return (
+      // Semantic HTML xyz
+      <>
+        <header className= {`${font.className} links-parent`}>
+            <ul className="links">
+                <li className="link"><Link href="">Works</Link></li>
+                <li className="link"><Link href="">Blog</Link></li>
+                <li className="link"><Link href="">Contact</Link></li>
+            </ul>
+        </header>
+      </>
+    );
+  }
   
